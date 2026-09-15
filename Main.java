433462085087ebc4e522bab7c20f84533a219677
tit-exe <titouane.bloda@gmail.com>
2026-09-15 16:42:14 +0200

    aura

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
  public static void main(String[] args) {
- System.out.println("Hello from GitHub and from a new branch!");
+ System.out.println("Projet collaboratif - TP2 (version B)");
  }
 }
