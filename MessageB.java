66e424342fb6874071b5690c7a7cd71dcf804fbe
tit-exe <titouane.bloda@gmail.com>
2026-09-15 16:34:36 +0200

    ajout du 67

--- MessageB.java
+++ MessageB.java
@@ -1,5 +1,5 @@
 public class MessageB {
     public static void main(String[] args) {
-        System.out.println("Message du membre B");
+        System.out.println("Message du membre B 67");
     }
 }
